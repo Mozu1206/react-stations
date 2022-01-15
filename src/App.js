c4ea71653a0636c0389f490590c47cc1c7a33f7e
Masayuki Takahashi <baskemasa1206@gmail.com>
2022-01-15 15:49:46 +0900

    React 03

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import './App.css'
  *
  * @type {React.FC}
  */
+
 export const App = () => {
   return (
     <div>
-      <header>Dogアプリ</header>
-      <h2>Hello, world!</h2>
+      <header>Dog App</header>
+      <p>This web page is for dog lovers</p>
+      <img src="https://images.dog.ceo/breeds/mountain-swiss/n02107574_2662.jpg" alt="A cute dog image" />
     </div>
   )
 }
